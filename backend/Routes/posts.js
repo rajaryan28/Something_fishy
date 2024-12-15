73bc544447c944ac9f2197be5da6749606fd8e8d
rajaryan2816 <rajaryan2816@gmail.com>
2024-12-15 15:52:01 +0530

    Backend Done.

--- backend/Routes/posts.js
+++ backend/Routes/posts.js
@@ -53,6 +53,67 @@ router.post(
   );
   
 
+// Route 3 : Update an existing  post of the specific user : Patch "/api/posts/updatepost". Login required
+
+router.patch("/updatepost/:id", fetchuser, async (req, res) => {
+  const post  = req.body.post;
+
+  // updating the post
+  try {
+    const newPost = {}; //new note that has to be updated
+    if (post) {
+      newPost.post = post;
+    }
+    
+
+    //finding the note that should be updated
+
+    let posts = await Post.findById(req.params.id);
+    if (!posts) {
+      return res.status(404).send("Not found!");
+    }
+
+    if (posts.user.toString() !== req.user.id) {
+      return res.status(401).send("Not allowed!");
+    }
+
+    posts = await Post.findByIdAndUpdate(
+      req.params.id,
+      { $set: newPost },
+      { new: true }
+    );
+    res.json({ posts });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Some Internal error occured");
+  }
+});
+
+
+// Route 4 : Delete an existing  notes of the specific user : Delete "/api/notes/deletepost/:id". Login required
+
+router.delete("/deletepost/:id", fetchuser, async (req, res) => {
+  try {
+    //finding the note that should be deleted
+
+    let posts = await Post.findById(req.params.id);
+    if (!posts) {
+      return res.status(404).send("Not found!");
+    }
+
+    if (posts.user.toString() !== req.user.id) {
+      return res.status(401).send("Not allowed!");
+    }
+
+    posts = await Post.findByIdAndDelete(req.params.id);
+    res.json({ success: "The note is succesfully deleted!", post:posts });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Some Internal error occured");
+  }
+});
+
+
 
 
 module.exports=router
