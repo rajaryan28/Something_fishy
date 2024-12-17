435ecb3faf13f004f10d182e666f5a3576da7756
rajaryan2816 <rajaryan2816@gmail.com>
2024-12-17 22:06:50 +0530

    Completed with footers

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from './components/Navbar';
+import Footer from './components/footer';
 function App() {
   return (
     <>
        <BrowserRouter>
        <Navbar/>
         <center>Hello this is Raj aryan</center>
-
+       <Footer/>
         </BrowserRouter>
     </>
   );
---
+++ src/components/footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+      <footer className="footer">
+        <button><img width="25" height="25" src="https://img.icons8.com/fluency-systems-regular/50/home--v1.png" alt="home--v1"/></button>
+        <button>➕</button>
+        <button><img width="25" height="25" src="https://img.icons8.com/fluency-systems-regular/50/user-male-circle--v1.png" alt="user-male-circle--v1"/></button>
+      </footer>
+    </div>
+  )
+}
+
+export default Footer
